7ae80d64ad6011e47ad7eb4268ecedb74be33aa5
Yuki Takei <yuki@weseek.co.jp>
2020-07-25 22:13:38 +0000

    use eslint-config-weseek@1.0.5

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,9 @@
-const path = require('path');
-
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
     'weseek',
-    'weseek/react',
+    'weseek/typescript-next',
     'plugin:jest/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  overrides: [
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off',
-      },
-    },
   ],
   env: {
     jquery: true,
@@ -29,30 +18,9 @@ module.exports = {
     window: true,
   },
   plugins: [
-    '@typescript-eslint',
     'jest',
   ],
-  settings: {
-    // prevent import/no-unresolved
-    // see: https://qiita.com/282Haniwa/items/76d56a6a7e9d0db95a33
-    'import/resolver': {
-      typescript: {
-        config: path.join(__dirname, './webpack.config.js'),
-        alwaysTryTypes: true,
-      },
-    },
-  },
   rules: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
     indent: [
       'error',
       2,
@@ -64,13 +32,5 @@ module.exports = {
         FunctionExpression: { body: 1, parameters: 2 },
       },
     ],
-    'react/jsx-filename-extension': [
-      'error',
-      {
-        extensions: ['.tsx'],
-      },
-    ],
-    // It's not required since Next.js will automatically add `import React from 'react'`
-    'react/react-in-jsx-scope': 'off',
   },
 };
