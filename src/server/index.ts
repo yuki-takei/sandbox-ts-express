f680a71b6c36cbecf4c80015b48e4efb84f555e0
Yuki Takei <yuki@weseek.co.jp>
2020-07-26 19:12:43 +0000

    set error handler to server

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,9 +6,28 @@ const app = next({ dev });
 const handle = app.getRequestHandler();
 const port = process.env.PORT || 3000;
 
+function installErrorHandler(app: any) {
+  const _renderErrorToHTML = app.renderErrorToHTML.bind(app);
+
+  app.renderErrorToHTML = (err: any, req: any, res: any, pathname: any, query: any) => {
+    if (err) {
+      console.error(err);
+    }
+
+    return _renderErrorToHTML(err, req, res, pathname, query);
+  };
+
+  return app;
+}
+
 async function main() {
   try {
     await app.prepare();
+
+    // TODO: set error handler
+    // https://github.com/vercel/next.js/issues/1852#issuecomment-353671222
+    installErrorHandler(app);
+
     const server = express();
     server.all('*', (req: Request, res: Response) => {
       return handle(req, res);
